2383ddc6d21a8969299bcc5600566450e266ea71
rookie <rookie@ganguo.hk>
2019-03-16 00:02:24 +0800

    0.0.1

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <img alt="Vue logo" src="./assets/logo.png">
     hello
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <IvueButton>asda</IvueButton>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 
 import Vue from 'vue'
 import App from './App.vue'
+import IvueMaterial from 'ivue-material';
 
+Vue.use(IvueMaterial);
 Vue.config.productionTip = false
 
+import 'ivue-material/dist/styles/ivue.css';
+
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
