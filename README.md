19a738c13c5e1446d56d1b7e8ef4f271c77b35f8
qq282126990 <282126990@qq.com>
2019-03-18 18:05:24 +0800

    no message

--- README.md
+++ README.md
@@ -6,8 +6,8 @@
 # 安装工程依赖
 npm install
 
-# 在本地启动调试 server
-npm run server
+# 在本地启动调试 serve
+npm run serve
 
 # 构建线上生产环境产物
 npm run build
--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    hello
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <IvueButton>asda</IvueButton>
   </div>
 </template>
 
