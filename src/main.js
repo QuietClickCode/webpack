2e2cc241504d8b1dfb099df4c6eb9e82bc4282dc
qq282126990 <282126990@qq.com>
2019-08-15 10:30:40 +0800

    修改 main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 
 import Vue from 'vue'
 import App from './App.vue'
-import IvueMaterial from 'ivue-material';
+import IvueMaterial from 'ivue-material'
+import 'ivue-material/dist/styles/ivue.css'
 
 Vue.use(IvueMaterial);
 Vue.config.productionTip = false
-import 'ivue-material/dist/styles/ivue.css';
+
 new Vue({
     render: h => h(App),
 }).$mount('#app')
